b01bb391088d3d978ea6ad1835806d02f42f4125
jamong-mini <jamong.alsl@gmail.com>
2024-10-28 12:30:35 +0900

    feat: create and complete Playground file for 1-2 CamelCase assignment
    - 작성일: 2024.10.28 (월)
    - 1-2 CamelCase 과제용 Playground 파일 생성 및 변수 정의 완료

---
+++ 1_2_Canmelcase.playground/Contents.swift
@@ -0,0 +1,48 @@
+// Playground file for 1-2 Canmelcase Assignment
+// 작성일: 2024.10.28 (월요일)
+// 이 파일은 1-2 Canmel Case 과제를 작성하고 연습을 포함합니다.
+//
+// 작성자: Jamong
+// 과제 설명:
+// - 해당 파일에서는 CanmelCase 표기법을 사용하는 방법을 연습합니다.
+
+import UIKit
+
+// 2. 주석 달기
+/* 실습 코드 주석 달기 */
+
+// 3. Camel Case로 되어있는 단어를 10개 이상 만들기
+let myNameIsJamong: String = "my name is Jamong"
+
+let helloWorld: String = "Hello World"
+
+let myCarIsFast: String = "my car is fast"
+
+let sheLovesCoding: String = "she loves coding"
+
+let weAreLearningSwift: String = "we are learing swift"
+
+let theSkyIsBlue: String = "the sky is blue"
+
+let iLikeCoffee: String = "i like coffee"
+
+let myFavoriteColorIsRed: String = "my favorite color is red"
+
+let thisIsATest: String = "this is a test"
+
+let programmingIsFun: String = "programming is fun"
+
+let appleMakesGreatProducts: String = "apple makes great products"
+
+let teamworkIsImportant: String = "teamwork is important"
+
+let theCatIsSleeping: String = "the cat is sleeping"
+
+let iWantToTravel: String = "i want to travel"
+
+let weAreDevelopers: String = "we are developers"
+
+let learningSwiftIsEasy: String = "learning swift is easy"
+
+let myComputerIsNew: String = "my computer is new"
+
